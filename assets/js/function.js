eb18cf08deae11b0574904eb1ffaabc08d81b6b5
Nico <nsilvaf94@gmail.com>
2024-05-16 19:56:29 -0400

    Agregar animaciones a logo y redes sociales

---
+++ assets/js/function.js
@@ -0,0 +1,47 @@
+function cambiarColor(e)
+{
+if(e.type=="mouseover")
+   e.currentTarget.style.color = 'grey';
+else
+   e.currentTarget.style.color = 'white';
+}
+let logo1 = document.getElementById('logo1');
+logo1.addEventListener('mouseover', cambiarColor);
+logo1.addEventListener('mouseout', cambiarColor);
+
+function cambiarColor(e)
+{
+if(e.type=="mouseover")
+   e.currentTarget.style.color = 'grey';
+else
+   e.currentTarget.style.color = 'white';
+}
+let logo2 = document.getElementById('logo2');
+logo2.addEventListener('mouseover', cambiarColor);
+logo2.addEventListener('mouseout', cambiarColor);
+
+function cambiarColor(e)
+{
+if(e.type=="mouseover")
+   e.currentTarget.style.color = 'grey';
+else
+   e.currentTarget.style.color = 'white';
+}
+let logo3 = document.getElementById('logo3');
+logo3.addEventListener('mouseover', cambiarColor);
+logo3.addEventListener('mouseout', cambiarColor);
+
+function cambiarColor(e)
+{
+if(e.type=="mouseover")
+   e.currentTarget.style.color = 'grey';
+else
+   e.currentTarget.style.color = 'white';
+}
+let logo4 = document.getElementById('logo4');
+logo4.addEventListener('mouseover', cambiarColor);
+logo4.addEventListener('mouseout', cambiarColor);
+
+
+
+
